43995f8c58de5736cddeaac4211f7021af60d8dc
priyankavagadre <priyankavagadre@hotmail.com>
2025-07-10 10:52:28 +0530

    GET all API implemented

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+
+const students = require('./MOCK_DATA.json');
+
 const app = express();
 const port = 3000;
+
+//Routes
+
+app.get('/students', (req,res)=>{
+  return res.json(students);
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
