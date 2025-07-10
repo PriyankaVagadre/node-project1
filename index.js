e5ba1bb55791b0b9679768fc433de851d17e2a45
priyankavagadre <priyankavagadre@hotmail.com>
2025-07-10 11:31:11 +0530

    group get,put and delete in 1 method

--- index.js
+++ index.js
@@ -20,26 +20,43 @@ app.get('/api/students', (req,res)=>{
   return res.json(students);
 })
 
-app.get('/api/students/:id', (req,res)=>{
-  const id = Number(req.params.id);
-  const student = students.find((student)=> student.id === id);
-  return res.json(student);
-})
-
 app.post('/api/students', (req,res)=>{
   //POST 1 student
   return res.json({status: 'POST Pending'})
 })
 
-app.patch('/api/students/:id', (req,res)=>{
-  //PATCH 1 student
-  return res.json({status: 'PATCH Pending'})
-})
+// app.get('/api/students/:id', (req,res)=>{
+//   const id = Number(req.params.id);
+//   const student = students.find((student)=> student.id === id);
+//   return res.json(student);
+// })
 
-app.post('/api/students/:id', (req,res)=>{
-  //DELETE 1 student
-  return res.json({status: 'DELETE Pending'})
-})
+// app.patch('/api/students/:id', (req,res)=>{
+//   //PATCH 1 student
+//   return res.json({status: 'PATCH Pending'})
+// })
+
+// app.delete('/api/students/:id', (req,res)=>{
+//   //DELETE 1 student
+//   return res.json({status: 'DELETE Pending'})
+// })
+
+//group the top 3 method in one
+
+app.route('/api/students/:id')
+.get((req,res)=>{
+    const id = Number(req.params.id);
+    const student = students.find((student)=> student.id === id);
+    return res.json(student);
+  })
+.patch((req,res)=>{
+    //PATCH 1 student
+    return res.json({status: 'PATCH Pending'})
+  })
+.delete((req,res)=>{
+    //DELETE 1 student
+    return res.json({status: 'DELETE Pending'})
+  })
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
